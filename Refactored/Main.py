0b38e4582d388aab0356c1f0637f0354e13ca397
Daniel <coimbrasantos.d@gmail.com>
2020-06-02 23:57:55 -0300

    Add root page handler

---
+++ Refactored/Main.py
@@ -0,0 +1,10 @@
+from wsgiref.simple_server import make_server
+
+from root import root
+
+app = root
+with make_server('', 8000, app) as httpd:
+    
+    print('\nServing on port 8000...''\nTo finish the application process, press Ctrl+C\n\n')
+   
+    httpd.serve_forever()
---
+++ Refactored/root.py
@@ -0,0 +1,27 @@
+def root(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type','text/html')]
+
+    html = """
+        <!DOCTYPE HTML>
+        <head>
+            <title>Music Store</title>
+            <!-- CSS only -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+            <!-- JS, Popper.js, and jQuery -->
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        </head>
+        <html>
+            <body>        
+                <p style="font-size:90px;text-align:center">Music Store</p>
+                <br>
+                <a href="http://127.0.0.1:8000" class="button" style="color:green;padding:15px 32px;text-align:center;font-size:50px;">Home</a>
+                <a href="http://127.0.0.1:8000/tracks"><p style="font-size:38px">Tracks</p></a><a href="http://127.0.0.1:8000/artists"><p style="font-size:38px">Artists</p></a>
+            </body>
+        </html>"""
+    start_response(status, response_headers)
+    result = html.encode('utf-8')
+    return [result]
