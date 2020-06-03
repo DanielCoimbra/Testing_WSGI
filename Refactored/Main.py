e40a47a312abde12f3a0fb397e5e8715f7e0df07
Daniel <coimbrasantos.d@gmail.com>
2020-06-02 23:34:28 -0300

    App launcher

---
+++ Refactored/Main.py
@@ -0,0 +1,7 @@
+from wsgiref import make_server
+
+with make_server('', 8000, app) as httpd:
+    
+    print('\nServing on port 8000...''\nTo finish the application process, press Ctrl+C\n\n')
+   
+    httpd.serve_forever()
