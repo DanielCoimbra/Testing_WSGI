12ec8831610e78e4e6677262de566689f43adf9a
Daniel <coimbrasantos.d@gmail.com>
2020-06-07 13:31:42 -0300

    Update main.py

--- Refactor/main.py
+++ Refactor/main.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 from wsgiref.simple_server import make_server
 from root import root_page
 from artists import artists_page
@@ -11,19 +12,19 @@ def route(environ, start_response):
     if path == "/" or path == '':
         return root_page(environ, start_response)
     elif path_sections[1] == 'tracks':
-        if len(path_sections) > 2:
+        if len(path_sections) > 2 :
             if path_sections[2] == '' or path_sections[2] == '/':
                 return all_tracks_page(environ, start_response)
             
-            elif len(path_sections) > 2 and int(path_sections[2]) >= 1 and int(path_sections[2]) < 3503:
+            elif int(path_sections[2]) >= 1 and int(path_sections[2]) < 3503:
                 return track_page(environ, start_response)
             
-            else: return error(environ, start_response)
-
+            else: return error_page(environ, start_response)
+        else: return all_tracks_page(environ, start_response)
     elif path_sections[1] == 'artists':
         return artists_page(environ,start_response)
     else:
-        return error(environ, start_response)
+        return error_page(environ, start_response)
     
 
 def launch_app():
