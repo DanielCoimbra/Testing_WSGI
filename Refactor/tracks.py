b18558cc63aaacfe66fae5db12e66f830d87b025
Daniel <coimbrasantos.d@gmail.com>
2020-06-07 13:29:54 -0300

    HTML text moved to an outside function, to decrease coupling as well as to access the function from another module without calling the whole app.

--- Refactor/tracks.py
+++ Refactor/tracks.py
@@ -12,7 +12,23 @@ def tracks_table():
     id_num = 0
     countLinks = 0
     html = """
-    
+        <!DOCTYPE HTML>
+        <head>
+            <title>Music Store</title>
+            <!-- CSS only -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            <!-- JS, Popper.js, and jQuery -->
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        </head>
+        <html>
+            <body>        
+                <p style="font-size:90px;text-align:center">Music Store</p>
+                <br>
+                <a href="http://127.0.0.1:8000" class="button" style="color:green;padding:15px 32px;text-align:center;font-size:50px;">Home</a>
+                <a href="http://127.0.0.1:8000/tracks/"><p style="font-size:38px">Tracks</p></a>
+                <br><br>
         <table class="table table-striped">
             <tr>
                 <th>TrackID</th>
@@ -45,35 +61,16 @@ def tracks_table():
                 html += "</a>"  # close link tag
             html += "</td>"  # Close cell tag
         html += "</tr>"
-    html += "</table>"
+    html += "</table></body></html>"
 
     return html
     
 def all_tracks_page(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type','text/html')]
-
-    html = """
-        <!DOCTYPE HTML>
-        <head>
-            <title>Music Store</title>
-            <!-- CSS only -->
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-            <!-- JS, Popper.js, and jQuery -->
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-        </head>
-        <html>
-            <body>        
-                <p style="font-size:90px;text-align:center">Music Store</p>
-                <br>
-                <a href="http://127.0.0.1:8000" class="button" style="color:green;padding:15px 32px;text-align:center;font-size:50px;">Home</a>
-                <a href="http://127.0.0.1:8000/tracks"><p style="font-size:38px">Tracks</p></a
-                <br><br>
-                {}
-            </body>
-        </html>""".format(tracks_table())
     start_response(status, response_headers)
+
+    html = tracks_table()
     result = html.encode('utf-8')
+    
     return [result]
