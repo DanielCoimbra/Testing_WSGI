86ca424bb5b62d989a0a4eb750c06bef30a7f1a7
Daniel <coimbrasantos.d@gmail.com>
2020-07-17 16:54:42 -0300

    Update route.py

--- refactor/route.py
+++ refactor/route.py
@@ -4,7 +4,6 @@ from root import root_page
 from artists import artists_page
 from tracks import all_tracks_page
 from track_page import track_page
-from error import error_page
 from werkzeug.routing import Map, Rule, NotFound, RequestRedirect
 from werkzeug.wrappers import Request
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
